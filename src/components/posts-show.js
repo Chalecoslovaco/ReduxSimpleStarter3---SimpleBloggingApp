fe520cb8b1fdd34da0892457d1f63681cfbf1914
Sergio <sergiogarher@gmail.com>
2018-07-17 21:09:25 +0200

    Add some navigation

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPostById } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsShow extends Component {
     componentDidMount() {
@@ -10,11 +11,12 @@ class PostsShow extends Component {
 
     render() {
         const { post } = this.props;
-        
+
         if(!post) return <div>Loading...</div>;
 
         return (
             <div>
+                <Link to="/">Back to Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>

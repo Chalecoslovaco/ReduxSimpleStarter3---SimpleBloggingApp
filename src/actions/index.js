a675fe52ab2534c184929b6d05593850c21b21a5
Sergio <sergiogarher@gmail.com>
2018-07-17 19:58:33 +0200

    Create fetchPostById action creator + add new case to PostsReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST_BY_ID = 'FETCH_POST_BY_ID';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=KEY1234';
 
@@ -24,4 +25,13 @@ export function createPost(values, callback) {
         type: FETCH_POSTS,
         payload: request
     };
+}
+
+export function fetchPostById(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_POST_BY_ID,
+        payload: request
+    }
 }
